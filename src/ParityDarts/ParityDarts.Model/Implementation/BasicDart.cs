4de6d52c0be713a9dd06cd2f9b4cd1aee65e4461
James Oddy <james@oddyitsolutions.co.uk>
2011-07-26 18:11:42 +0100

    Initial Commit

---
+++ src/ParityDarts/ParityDarts.Model/Implementation/BasicDart.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ParityDarts.Model.Implementation
+{
+    public class BasicDart : IDart
+    {
+        public int Points
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int PointsRemaining
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public DartResult Result
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ src/ParityDarts/ParityDarts.Model/Interfaces/IDart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ParityDarts.Model
+{
+    public interface IDart
+    { 
+        int Points { get; set; }
+        int PointsRemaining { get; set; }
+        DartResult Result { get; set; }
+    }
+}
