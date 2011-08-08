b6719a18827193936accc888776c777ced201a93
James Oddy <james@oddyitsolutions.co.uk>
2011-08-08 17:43:24 +0100

    Added nunit binaries Pointed ParityDarts.Model.Test.csproj post build to run nunit

---
+++ src/ParityDarts.Contracts/Legs/ILeg.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ParityDarts.Contracts
+{
+    public interface ILeg
+    {
+        ICollection<IThrow> Throws { get; }
+        LegResult Result { get; }
+        IPlayer Player { get; }
+    }
+}
